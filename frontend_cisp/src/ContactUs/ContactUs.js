5736b95f0954336a053a6f430f25060b6d52d66d
marasinipradeep <marasinipradeep@gmail.com>
2021-03-23 16:17:37 +1030

    added form on contact us

--- frontend_cisp/src/ContactUs/ContactUs.js
+++ frontend_cisp/src/ContactUs/ContactUs.js
@@ -1,6 +1,26 @@
-import SocialLinks from '../PureComponents/SocialLinks/SocialLink';
-export default function Contactus(props) {
 
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Email from '@material-ui/icons/Email';
+import WrapText from '@material-ui/icons/WrapText';
+import SocialLinks from '../PureComponents/SocialLinks/SocialLink';
+
+const useStyles = makeStyles((theme) => ({
+    margin: {
+        margin: theme.spacing(1),
+    },
+}));
+
+export default function Contactus(props) {
+    const classes = useStyles();
 
     return (
         <div className="container-fluid">
@@ -13,17 +33,48 @@ export default function Contactus(props) {
                 </div>
 
                 <div className="col-sm-12 col-md-6 jumbotron">
-                    <input placeholder="Name"  className="m-4"/>
-                    <input placeholder="Email" className="m-4" /><br/>
-                    <input placeholder="Subject" className="m-4" /><br/>
-                    <textarea placeholder="Please Enter Your Message" className="m-4"></textarea>
-                    <button className="m-4">Submit</button>
+
+
+                    <div className={classes.margin}>
+                        <Grid container spacing={1} alignItems="flex-end">
+                            <Grid item>
+                                <AccountCircle />
+                            </Grid>
+                            <Grid item>
+                                <TextField id="input-with-icon-grid" label="Your Name" />
+                            </Grid>
+                            <Grid item>
+                                <Email />
+                            </Grid>
+                            <Grid item>
+                                <TextField id="input-with-icon-grid" label="Email" />
+                            </Grid>
+                        </Grid>
+                    </div>
+                    <div className={classes.margin}>
+                        <Grid container spacing={1} alignItems="flex-end">
+
+                            <Grid item>
+                                <TextField id="outlined-basic" label="Your Message" variant="outlined" />
+                            </Grid>
+                        </Grid>
+                    </div>
+
+                    <div className={classes.margin}>
+                        <Grid container spacing={2} alignItems="flex-end">
+
+                            <Grid item>
+                                <Button variant="contained" color="primary" component="span">
+
+
+                                    Submit
+        </Button>
+                            </Grid>
+                        </Grid>
+                    </div>
 
                 </div>
-
-
             </div>
-
         </div>
     )
 }
