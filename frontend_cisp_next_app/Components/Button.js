57e80739fa696baa4684c3dda59439ac86719f3f
marasinipradeep <marasinipradeep@gmail.com>
2021-03-26 14:08:07 +1030

    Hero image with responsive test completed

--- frontend_cisp_next_app/Components/Button.js
+++ frontend_cisp_next_app/Components/Button.js
@@ -8,12 +8,61 @@ export default function Button(props) {
 }
 
 const Buttons = styled.button`
-  color:#FFFFFF ;
-  border: 2px solid red;
-  background: #63C672;
-  font-size: 1em;
-  margin:0px;
-  padding:10px 20px;
-
+padding: 0.5rem 2rem;
+margin-right:5px;
+display: flex;
+align-items: center;
+position: relative;
+text-transform: uppercase;
+font-weight: 500;
+font-size: 1em;
+background: #63C672;
+border: 0;
+cursor: pointer;
+color: #FFFFFF;
+@media (max-width: 700px) {
+  font-size: 10px;
+  padding: 0 10px;
+}
+&:before {
+  content: '';
+  width: 2px;
+  background: var(--lightGrey);
+  height: 100%;
+  left: 0;
+  position: absolute;
+  transform: skew(-20deg);
+  top: 0;
+  bottom: 0;
+}
+&:after {
+  height: 2px;
+  background: red;
+  content: '';
+  width: 0;
+  position: absolute;
+  transform: translateX(-50%);
+  transition: width 0.4s;
+  transition-timing-function: cubic-bezier(1, -0.65, 0, 2.31);
+  left: 50%;
+  margin-top: 2rem;
+}
+&:hover,
+&:focus {
+  outline: none;
+  &:after {
+    width: calc(100% - 60px);
+  }
+  @media (max-width: 700px) {
+    width: calc(100% - 10px);
+  }
+}
+}
+@media (max-width: 1300px) {
+border-top: 1px solid var(--lightGrey);
+width: 100%;
+justify-content: center;
+font-size: 1.5rem;
+}
   
 `;
--- frontend_cisp_next_app/Components/HeroImage.js
+++ frontend_cisp_next_app/Components/HeroImage.js
@@ -2,21 +2,30 @@ import LazyHero from 'react-lazy-hero';
 import Styled from "styled-components";
 import Button from "./Button";
 export default function Heroimage(props) {
-    
+
 
     return (
-        <>
-            <LazyHero imageSrc="https://unsplash.it/2000/1000">
-                <h1> Creative Informative Supportive Partners</h1>
-                <br/>
-                <br/>
-                <br/>
-                <Button>Services</Button>
-                <Button>Get Quote</Button>
+       
+            <LazyHero imageSrc="https://unsplash.it/2000/1000" isFixed={true} color="#010101" minHeight="80vh">
+                
+                <HeroHeading>We provide consultation and manage from start to end.</HeroHeading>
+                <br />
+                <br />
+                <br />
+                <HeroButton>
+                    <Button>OUR SERVICES</Button>
+                    <Button>GET A QUOTE</Button>
+                    </HeroButton>
             </LazyHero>
-        </>
+            
     )
 }
 
-const HeroImage = Styled.div`
+const HeroHeading = Styled.h1`
+color: #FFFFFF;
+text-transform: uppercase;
+`;
+
+const HeroButton = Styled.div`
+display:flex;
 `;
