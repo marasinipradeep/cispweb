690c5557bfb185fcbed0e7c12e669f2e253a5297
marasinipradeep <marasinipradeep@gmail.com>
2021-03-27 11:28:23 +1030

    nprogress added on change of routes

--- frontend_cisp_next_app/pages/_app.js
+++ frontend_cisp_next_app/pages/_app.js
@@ -1,8 +1,18 @@
 // add bootstrap css 
+import NProgress from "nprogress";
+import Router from 'next/router';
+
+import 'nprogress/nprogress.css';
+
 import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css';
 import "../Components/styles/Navbar.css"
 import Page from '../Components/Page';
+
+
+Router.events.on('routeChangeStart',()=>NProgress.start());
+Router.events.on('routeChangeComplete',()=>NProgress.done());
+Router.events.on('routeChangeError',()=>NProgress.done());
 
  function MyApp({ Component, pageProps }) {
   return(
